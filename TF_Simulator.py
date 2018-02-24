9896f65c33573bd7b9f83da190cd1f917c2a3c9c
keita-f-sato <sts136-keita-f-sato@users.noreply.github.com>
2018-02-24 19:27:54 +0900

    change program structure

---
+++ TF_Simulator.py
@@ -0,0 +1,107 @@
+import socket
+from contextlib import closing
+import numpy as np
+import pygame
+from pygame.locals import *
+import concurrent.futures
+import Convert_format
+import Transfer_function_plane
+import sys
+import pandas as import pd
+
+pygame.joystick.init()
+try:
+    j = pygame.joystick.Joystick(0) # create a joystick instance
+    j.init() # init instance
+
+except pygame.error:
+    print ('Joystickが見つかりませんでした。')
+
+#xplane送信フォーマット用リスト
+first_DATA = [68, 65, 84, 65, 0, 11, 0, 0, 0]
+filght_con_DATA = [0, 192, 121, 196, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+throttle_con1_DATA = [25, 0, 0, 0]
+throttle_con2_DATA = [0]*28
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+host = '127.0.0.1'
+port = 49007
+sedport = 49000
+bufsize = 1024
+DATA_Xplane = [0]*72#初期データ
+Auto_comand_bin = []
+ip_sp = 0
+delta_A = np.asarray([0,0,0,0])
+delta_baffer = np.asarray([0,0,0,0])#微分計算用バッファ
+stop_program = True
+
+def read_xplane_DATA():
+  global DATA_Xplane,stop_program
+  with closing(sock):
+    sock.bind((host, port))
+    while stop_program == True:
+      recive_xplane_data = sock.recv(bufsize)
+      recive_xplane_data_int = [int(recive_xplane_data[i]) for i in range(5,len(recive_xplane_data))]
+      recive_xplane_data_bin =  [bin(recive_xplane_data_int[ii]).zfill(8) for ii in range(0,len(recive_xplane_data_int))]
+      Flip_bin_DATA = recive_xplane_data_bin[::-1]
+      DATA_Xplane = Convert_format.convert_IEEE2dec(Flip_bin_DATA)
+
+  return
+
+
+def read_joystick_DATA():
+    pygame.init()
+    global delta_baffer,stop_program
+    TF_start_switch = 1#初期スイッチ（joystickスルー）
+    while stop_program == True:
+        for e in pygame.event.get(): # イベントチェック
+
+            x9 , y9 = 0.999*j.get_axis(0), 0.999*j.get_axis(1)
+            x10 , y10 = -0.999 * j.get_axis(3), 0.999*j.get_axis(4)
+            RL = j.get_axis(2)
+            throttecon_DATA = 0.5*x10+0.5
+            filcon_DATA = [y9, x9, y10, throttecon_DATA] #[elv/ail/rud]←リトルエンディアンなので逆
+
+            if  e.type == pygame.locals.JOYBUTTONDOWN:
+                if e.button == 1:
+                    TF_start_switch += 1
+                elif e.button == 7:
+                    stop_program = False
+
+        if TF_start_switch & 1:#joystickのコマンドをXplaneにスルー出力
+            control_comand_bin = [Convert_format.convert_Dec2bin(filcon_DATA[i4]) for i4 in range(0,4)]
+            send_xplane_DATA(control_comand_bin)
+
+        else:#Auto制御を実施
+            D_delta_a = Dff(delta_baffer)#微分値計算
+            Auto_comand_dec = Transfer_function_plane.transfer_fuction(DATA_Xplane,filcon_DATA,D_delta_a)#制御コマンドの計算
+            delta_baffer = Auto_comand_dec
+            Auto_comand_bin = [Convert_format.convert_Dec2bin(Auto_comand_dec[i4]) for i4 in range(0,4)]
+            print(Auto_comand_dec)
+
+            send_xplane_DATA(Auto_comand_bin)
+
+
+def send_xplane_DATA(filcon):
+
+    senddata = bytes(first_DATA + filcon[0]+filcon[1]+filcon[2] + \
+               filght_con_DATA + throttle_con1_DATA + filcon[3] + throttle_con2_DATA)#Xplane送信フォーマットに代入
+
+    sock.sendto(senddata, (host, sedport))
+
+def Save_flight_data(flight_data):
+
+
+def Dff (delta_a):#微分関数
+    global delta_A
+    delta_a_D = delta_a - delta_A
+    delta_A = np.asarray(delta_a)
+    return delta_a_D
+
+
+if __name__ == '__main__':
+  executor = concurrent.futures.ThreadPoolExecutor(max_workers=2)
+  executor.submit(read_xplane_DATA)
+  executor.submit(read_joystick_DATA)
+  #read_joystick_DATA()
+  #read_xplane_DATA()
--- Transfer_function_plane.py
+++ Transfer_function_plane.py
@@ -1,3 +1,6 @@
+# -*- coding: utf-8 -*-
+
+
 import numpy as np
 import math
 
@@ -9,20 +12,24 @@ def transfer_fuction(xplane2dec,control_DATA,delta_B):
     T_roll = 1
     T_pitch = 1
     g = 9.8 #m/s**2
-    K_i = [1, 1, 1, 1]
-    K_p = [0.2, 0.2, 0.2, 0.2]
-    K_ff = [1, 0.5, 0.5, 0.5]
+    K_i = np.asarray([1, 1, 1, 1])
+    K_p = np.asarray([0.2, 0.5, 1e-10, 0.2])
+    K_ff = np.asarray([0.3, 0.5, 0.5, 0.5])
+    buffer_a = 1e-10
 
 
 
 
-    Phi = float(xplane2dec[42]) + 0.0001    # roll
-    Theta = float(xplane2dec[43]) + 0.0001  #pitch
-    Psi = float(xplane2dec[41]) + 0.0001    #yaw
-    u = float(xplane2dec[13]) + 0.0001
-    w = float(xplane2dec[11]) + 0.0001
-    p = float(xplane2dec[51]) + 0.0001
-    h = float(xplane2dec[23]) + 0.0001
+    Phi = float(xplane2dec[42]) + buffer_a    # roll
+    Theta = float(xplane2dec[43]) + buffer_a  #pitch
+    Psi = float(xplane2dec[41]) + buffer_a    #yaw
+    u = float(xplane2dec[13]) + buffer_a #x acxis speed
+    v = float(xplane2dec[12]) + buffer_a #y acxis speed
+    w = float(xplane2dec[11]) + buffer_a #z acxis speed
+    p = float(xplane2dec[51]) + buffer_a #roll moment
+    q = float(xplane2dec[52]) + buffer_a #roll moment
+    r = float(xplane2dec[50]) + buffer_a #roll moment
+    h = float(xplane2dec[23]) + buffer_a  #altitude
 
 
 #moment sp
@@ -43,9 +50,69 @@ def transfer_fuction(xplane2dec,control_DATA,delta_B):
 
     delta = [delta_e, delta_a, delta_r, delta_th]
 
-    print(r_sp)
+
+    R = create_state_array(Phi,Theta,Psi)
+    R_sp = create_state_array(Phi_sp,Theta_sp,Psi_sp)
+
+    R_z = np.asarray([R[0,2],R[1,2],R[2,2]])
+    R_z_sp =np.asarray([R_sp[0,2],R_sp[1,2],R_sp[2,2]])
+
+    C = np.cross(R_z,R_z_sp)
+    e_R = np.dot(R.T,C)
+
+    cos = np.dot(R_z , R_z_sp)
+    sin = np.linalg.norm(e_R)
 
 
+    tan = sin / cos
+    angle = np.arctan(tan)
+    axis = np.matrix(e_R) / sin
+    e_R = axis * angle
+
+
+    cp1 =np.asarray([0 , 0 , axis[0,1] ])
+    cp2 =np.asarray([0, 0 , -1*axis[0,0] ])
+    cp3 =np.asarray([-1*axis[0,1] , axis[0,0] , 0 ])
+
+    e_R_cp = np.matrix([cp1,cp2,cp3])
+
+    cos_theta = np.cos(angle)
+    sin_theta = np.sin(angle)
+
+    A = np.matrix([[1,0,0],[0,1,0],[0,0,1]])
+    R_rp =R *(A + (e_R_cp * sin + (e_R_cp*e_R_cp*(1-cos) )))
+
+
+
+    #yaw
+    R_x_sp = np.asarray([1,0,0])
+    R_x_rp = np.asarray([R_rp[0,0],R_rp[1,0],R_rp[2,0]])
+
+    sinx = np.dot(np.cross(R_x_rp,R_x_sp),R_z_sp)
+    cosx = np.dot(R_x_rp,R_x_sp)
+    tanx = sinx/cosx
+
+    yaw_w = A[2,2] * A[2,2]
+
+    e_R[0,2] = np.arctan(tanx) * yaw_w
+
+    eP = np.matrix([1,1,1])
+    rates_sp = [e_R[0,0]*eP[0,0],e_R[0,1]*eP[0,1],e_R[0,2]*eP[0,2]]
+    rates = np.matrix([q,p,r])
+    rate_err = rates_sp - rates
+    att_control = K_p[0:3]*np.asarray(rate_err) + K_ff[0:3] * delta_B[0:3]
+    delta = [att_control[0,0],att_control[0,1],att_control[0,2], delta_th]
+    #print(delta)
+
+    #delta_2 = delta_a - float(att_control[0,1])
+    #print(delta_2)
 
 
     return delta
+
+def create_state_array(Phi,Theta,Psi):
+    F1 = np.asarray([np.cos(Theta)*np.cos(Psi), np.cos(Theta)*np.sin(Psi), -1 * np.sin(Theta)])
+    F2 = np.asarray([np.sin(Phi)*np.sin(Theta)*np.cos(Psi) - np.cos(Phi)*np.sin(Psi), np.sin(Phi)*np.sin(Theta)*np.sin(Psi) + np.cos(Phi)*np.cos(Psi), np.sin(Phi)*np.cos(Theta)])
+    F3 = np.asarray([np.cos(Phi)*np.sin(Theta)*np.cos(Psi) + np.sin(Phi)*np.sin(Psi), np.cos(Phi)*np.sin(Theta)*np.sin(Psi) - np.sin(Phi)*np.cos(Psi), np.cos(Phi)*np.cos(Theta)])
+    E = np.matrix([F1,F2,F3])
+    return E
